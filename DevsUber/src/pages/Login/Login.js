0c0a003cd257ec38b1df2a4064dbcdcb05113d4d
Leocaliban <leocaliban@gmail.com>
2019-12-10 14:35:09 -0300

    Cria menu de login e cadastro

--- DevsUber/src/pages/Login/Login.js
+++ DevsUber/src/pages/Login/Login.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Container,
   Header,
-  HeaderTitle
+  HeaderTitle,
+  Menu,
+  MenuItem,
+  MenuItemTitle
 } from './LoginStyle';
 
 const Page = () => {
+
+  const [activeMenu, setActiveMenu] = useState('signin');
+
   return (
     <Container>
       <Header>
-        <HeaderTitle>DevsUber</HeaderTitle>
+        <HeaderTitle>Uber</HeaderTitle>
       </Header>
+
+      <Menu>
+        <MenuItem
+          active={activeMenu === 'signin'}
+          onPress={() => setActiveMenu('signin')}
+          underlayColor="transparent"
+        >
+          <MenuItemTitle>Login</MenuItemTitle>
+        </MenuItem>
+        <MenuItem
+          active={activeMenu === 'signup'}
+          onPress={() => setActiveMenu('signup')}
+          underlayColor="transparent"
+        >
+          <MenuItemTitle>Cadastrar</MenuItemTitle>
+        </MenuItem>
+      </Menu>
     </Container>
   )
 }
--- DevsUber/src/pages/Login/LoginStyle.js
+++ DevsUber/src/pages/Login/LoginStyle.js
@@ -17,3 +17,19 @@ export const HeaderTitle = styled.Text`
   font-size: 27px;
 `;
 
+export const Menu = styled.View`
+  background-color: #3574CB;
+  flex-direction: row;
+  padding-left: 20px;
+`;
+
+export const MenuItem = styled.TouchableHighlight`
+  padding: 20px;
+  border-bottom-width: 5px;
+  border-bottom-color: ${props => props.active ? '#FFFFFF' : '#3574CB'};
+`;
+
+export const MenuItemTitle = styled.Text`
+  color: #FFFFFF;
+  font-size: 16px;
+`;
