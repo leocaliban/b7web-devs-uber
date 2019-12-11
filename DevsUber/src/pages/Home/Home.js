c05032336ceee35c857c67f149b7e540658bfa0f
Leocaliban <leocaliban@gmail.com>
2019-12-11 16:58:50 -0300

    Configura Geocoder e Geolocation

--- DevsUber/src/pages/Home/Home.js
+++ DevsUber/src/pages/Home/Home.js
@@ -1,14 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import {
     StatusBar,
-    View,
-    Text
 } from 'react-native';
 
 import { Container } from './HomeStyle';
 
 import MapView from 'react-native-maps';
+
+import Geolocation from '@react-native-community/geolocation';
+import Geocoder from 'react-native-geocoding';
+
+import { MAPS_API } from '../../configs/config';
 
 const Page = () => {
     const map = useRef();
@@ -23,6 +26,39 @@ const Page = () => {
         altitude: 0,
         heading: 0
     });
+
+    const [fromLocation, setFromLocation] = useState({});
+    const [toLocation, setToLocation] = useState({});
+
+    useEffect(() => {
+        Geocoder.init(MAPS_API, { language: 'pt-br' });
+        getMyCurrentPosition();
+    }, []);
+
+    const getMyCurrentPosition = () => {
+        Geolocation.getCurrentPosition(async (info) => {
+
+            const geo = await Geocoder.from(info.coords.latitude, info.coords.longitude);
+            if (geo.results.lenght > 0) {
+                const location = {
+                    name: geo.results[0].formatted_address,
+                    center: {
+                        latitude: info.coords.latitude,
+                        longitude: info.coords.longitude
+                    },
+                    zoom: 19,
+                    pitch: 0,
+                    altitude: 0,
+                    heading: 0
+                };
+
+                setMapLocal(location);
+                setFromLocation(location);
+            }
+        }, (error) => {
+
+        });
+    };
 
     return (
         <Container>
