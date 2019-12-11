274578408ef67922a0b9a91e1e7862af6cc57159
Leocaliban <leocaliban@gmail.com>
2019-12-11 15:28:44 -0300

    Define localização do mapa

--- DevsUber/src/pages/Home/Home.js
+++ DevsUber/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import {
     StatusBar,
@@ -12,6 +12,18 @@ import MapView from 'react-native-maps';
 
 const Page = () => {
     const map = useRef();
+
+    const [mapLocal, setMapLocal] = useState({
+        center: {
+            latitude: -7.23537,
+            longitude: -35.8756552
+        },
+        zoom: 19,
+        pitch: 0,
+        altitude: 0,
+        heading: 0
+    });
+
     return (
         <Container>
             <StatusBar barStyle="dark-content"></StatusBar>
@@ -19,6 +31,7 @@ const Page = () => {
                 ref={map}
                 style={{ flex: 1 }}
                 provider="google"
+                camera={mapLocal}
             ></MapView>
         </Container>
     );
